f53a0725edc3070d3ac012fdcadf53850110e249
Sudhir Mishra <admin@innit.social>
2026-05-29 16:13:52 +0100

    Fixed search import

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -3,9 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/providers/favorites_provider.dart';
 import 'package:meals/providers/filters_provider.dart';
-import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
+import 'package:meals/screens/search.dart';
 import 'package:meals/screens/shopping_list.dart';
 import 'package:meals/widgets/main_drawer.dart';
 import 'package:meals/screens/home.dart';
